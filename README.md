cb98d3c11b6a9b5000ac13bd9eb2ebe69415e75f
ChristopherGatt <110685887+ChristopherGatt@users.noreply.github.com>
2022-09-01 21:14:41 +1000

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Work Day Scheduler Starter Code
+
+First we review the starter code
+
+Next we use bootstrap to get curent day displayed
+
+Next we use bootstrap to get a sample row setup
+
+Next we add the time in left column
+
+Connect css to html file
+
+Now we use fontawesome to add Lock icon
+
+Now Add pst, present, future to middle column
+
+Next add click function to button
+
+Create a localStorage for Data from button click
+
+The saved data persists
--- script.js
+++ script.js
@@ -3,3 +3,57 @@ console.log(moment().format("dddd, MMMM Do"));
 var currentDayEl = document.getElementById("currentDay");
 console.log(currentDayEl);
 currentDayEl.textContent = moment().format("dddd, MMMM Do");
+
+var currentTime = moment().format("H");
+
+console.log(currentTime);
+
+var rowColourEl = document.getElementById("row");
+
+
+if ((rowColourE1 = currentTime)) {
+  rowColourEl = "present";
+} else if (rowColourE1 < currentTime) {
+  rowColourE1 = "past";
+} else {
+  rowColourE1 = "future";
+}
+
+var text = localStorage.getItem("text");
+
+
+"saveBtn".addeventListner("click",function(event) {
+  event.preventDefualt();
+  if (saveBtn) {
+    localStorage.setItem("text", text);
+  } 
+}
+
+
+
+/*const rows = document.getElementsByClassName("col-10");
+let currentHour = parseInt(moment().format('H'));
+
+Array.from("col-10").forEach(row => {
+  let
+    "col-10IdString" = "col-10.id",
+    rowHour;
+  if (rowIdString) {
+    rowHour = parseInt(rowIdString);
+  }
+  if (rowHour) {
+    // Compares row id to current hour and sets color accordingly
+    if (currentHour === rowHour) {
+      setColor(row, "red");
+    } else if ((currentHour < rowHour) && (currentHour > rowHour - 6)) {
+      setColor(row, "green");
+    } else if ((currentHour > rowHour) && (currentHour < rowHour + 6)) {
+      setColor(row, "lightgrey");
+    } else {
+      setColor(row, "white");
+    }
+  }
+});
+
+function setColor(element, color) {
+  element.style.backgroundColor = color;*/
